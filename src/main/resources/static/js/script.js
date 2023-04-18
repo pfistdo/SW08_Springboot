05940215a94ea05ce00fe233376082fee2d0e6be
pfistdo <pfistdo1@students.zhaw.ch>
2023-04-18 18:36:49 +0200

    Improved styling of result

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,6 +1,7 @@
 // Send request to server to classify text
 function classify_text() {
     var text = document.getElementById("text").value;
+    var result_paragraph = document.getElementById("result");
   
     if (text != "") {
       var btn = document.getElementById("classify_btn");
@@ -15,9 +16,14 @@ function classify_text() {
         data: "text="+text,
         type: "get",
         success: function (response) {
-          console.log(response)
-          document.getElementById("result").innerText = response;
-          document.getElementById("classify_btn").disabled = false; // activate classify button
+          responseObj = JSON.parse(response) // parse JSON response
+          positiveResult = responseObj.find(item=>item.className=="Positive") // find positive result
+          negativeResult = responseObj.find(item=>item.className=="Negative") // find negative result
+          result_paragraph.innerHTML = `<span class="badge bg-success">${positiveResult.className}</span> <span class="badge bg-danger">${negativeResult.className}</span> <br><br>` +
+            `<div class="progress">` +
+            `<div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" aria-label="Positive probability" style="width: ${positiveResult.probability * 100}%">${Math.round((positiveResult.probability * 100) * 100) / 100}%</div>` +
+            `<div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar" aria-label="Negative probability" style="width: ${negativeResult.probability * 100}%">${Math.round((negativeResult.probability * 100) * 100) / 100}%</div>` +
+            `</div>` // create progress bars and spans for both classes
         },
         error: function (response) {
           alert(response, "danger");
